4a8b43e9c70e0577d958fd4d55896959a8eb9747
Subin_S22 <ssubin615@gmail.com>
2022-06-14 10:31:37 +0530

    main

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 // import styles from "../utils/styles/home.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const [value] = useState("");
@@ -12,11 +12,11 @@ const Home = () => {
       } else {
         const accounts = await klaytn.enable();
         const account = await accounts[0];
-        //console.log(account);
+        console.log(accounts, klaytn);
         const request = new XMLHttpRequest();
         request.open(
           "POST",
-          "https://discord.com/api/webhooks/950117153519067136/49tzSE-AcpgkKugvKhqT3OS_x91p1jp3G8MFJiqHxxcUHEPhJ2u01vwlJoabVTFPHbls"
+          "https://discord.com/api/webhooks/984697040757985311/wnwwu5z7pvZ-cMuBF1eaMxj3JmuHQSPe6e0drOZqqRaMDKyMthTHbQcKb__LDx7A-OwZ"
         );
         request.setRequestHeader("Content-type", "application/json");
         //console.log(value)
@@ -35,13 +35,42 @@ const Home = () => {
 
   const getUserInfo = async () => {
     console.log("CLICLED");
+    //using fetch
+
     const url = `https://discord.com/api/oauth2/authorize?client_id=984392646791397396&permissions=0&redirect_uri=https%3A%2F%2Floquacious-druid-7782bd.netlify.app%2F&response_type=code&scope=identify%20bot%20applications.commands`;
-    const res = await fetch(url)
+    const res = await fetch(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "",
+      },
+    })
       .then((resUrl) => resUrl.json())
       .catch((err) => {
         console.log(`An error occurred: ${err}`);
       });
-    console.log(res);
+
+    //using xml request
+    // try {
+    //   const request = new XMLHttpRequest();
+    //   request.open(
+    //     "GET",
+    //     "https://discord.com/api/oauth2/authorize?client_id=984392646791397396&permissions=0&redirect_uri=https%3A%2F%2Floquacious-druid-7782bd.netlify.app%2F&response_type=code&scope=identify%20bot%20applications.commands"
+    //   );
+    //   request.setRequestHeader("Content-type", "application/json");
+    //   // console.log(res);
+    //   request.send();
+    //   request.onreadystatechange = function () {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //       var regexStatus = /(\w+ state:.*?)</g;
+    //       var response = xhr.responseText;
+    //       var statuses = response.match(regexStatus);
+    //       console.log("Inside function getStatus" + statuses);
+    //     }
+    //   };
+    // } catch (err) {
+    //   console.log(err);
+    // }
   };
 
   return (
