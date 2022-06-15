3c809fe899f0515e6493606ebfcb3c765c816586
Subin_S22 <ssubin615@gmail.com>
2022-06-15 17:02:42 +0530

    connect page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Provider from "../store/context";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
